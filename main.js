53e873be70f45c928e8d049ada8dbca4a960d69d
Luís Fernando Nascimento <105133507+LuisFNascimento@users.noreply.github.com>
2024-10-22 11:18:44 -0300

    Excluindo a classe teste, e adicionando a função Elemento não encontrado ou seletor inválido

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+ 
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-// ENQUANTO
-while (contador < 9){
+// PARA
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1]; 
     const idAudio = `#som_${instrumento}`;  //template string
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
     }
 
     tecla.onkeydown = function (evento) {
 
-        console.log(evento.code === 'Space');
-        if (evento.code === 'Space') {
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
         
